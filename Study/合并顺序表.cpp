2b5eb2abc927ec997e914b6d714ee3a83a7c5ce6
cashyuqi@gmail.com <cashyuqi@gmail.com>
2022-04-21 21:45:52 +0800

    add mergeList

---
+++ Study/合并顺序表.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+
+int *mergeList(int *l1,int *l2,int len1,int len2){
+    int *newArr = (int *)malloc(len1 + len2);
+    int k = 0, i = 0, j = 0;
+}
+
+int main(){
+    int l1[] = {1,3,5,8};
+    int l2[] = {2, 4, 4, 6, 7};
+    int len1 = sizeof(l1)/sizeof(int);
+    int len1 = sizeof(l2)/sizeof(int);
+    int *res;
+    return 0;
+}
