044972a94e9ed7819677d1c001af08865d3d5d6b
cashyuqi <cashyuqi@gmail.com>
2024-01-04 09:22:24 +0800

    更改

--- Study/HelloWorld.cpp
+++ Study/HelloWorld.cpp
@@ -1,5 +1,14 @@
 #include <stdio.h>
-int main(){
-    printf("hello world");
-    return 0;
+ 
+int main()
+ {
+     int i = 0;
+     while(i < 10)
+     {                                                                                                                                                                                                                          
+         printf("hello world!\n");
+         i++;
+     }
+      printf("\n");
+  
+      return 0;
 }
